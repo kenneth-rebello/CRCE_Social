a84c2b6b668672ea00cc8a3a0ea77a2567c104fb
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2019-12-17 13:04:52 +0530

    Server fix 2

--- server.js
+++ server.js
@@ -43,10 +43,6 @@ app.use(methodOverride('_method'));
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    });
 }
 
 //Define Routes
